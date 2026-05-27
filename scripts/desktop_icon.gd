8618c3aa10c1f6dec4fd9756cea60974c7992d50
etper <piotr.brzezinski9@gmail.com>
2026-05-27 13:17:01 +0200

    desktop icons

---
+++ scripts/desktop_icon.gd
@@ -0,0 +1,36 @@
+extends Button
+
+@export var app_scene: PackedScene
+
+var click_timer := 0.0
+var click_count := 0
+
+func _pressed():
+
+	click_count += 1
+
+	if click_count == 1:
+
+		click_timer = 0.25
+
+	else:
+
+		open_app()
+
+		click_count = 0
+
+func _process(delta):
+
+	if click_timer > 0:
+
+		click_timer -= delta
+
+		if click_timer <= 0:
+
+			click_count = 0
+
+func open_app():
+
+	var desktop = get_tree().get_first_node_in_group("desktop")
+
+	desktop.open_app(app_scene)
